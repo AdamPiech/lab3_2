ff97235ee402e89dd9550bb63b1cb7af48cbe25d
Adam Piech <adampiech7@op.pl>
2016-04-17 19:22:13 +0200

    Dodano test stanu dla wiadomości publicznych.

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -1,15 +1,24 @@
 package edu.iis.mto.staticmock;
 
 import static org.powermock.api.mockito.PowerMockito.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import edu.iis.mto.staticmock.reader.NewsReader;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
 public class NewsLoaderTest {
 
     private ConfigurationLoader configurationLoader;
+	private NewsReader newsReader;
 	
 	@Before
 	public void start() {
@@ -18,11 +27,23 @@ public class NewsLoaderTest {
 		mockStatic(ConfigurationLoader.class);
 		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
 		
-		NewsReader newsReader = mock(NewsReader.class);
-		when(newsReader.read()).thenReturn(new IncomingNews());
+		newsReader = mock(NewsReader.class);
+//		when(newsReader.read()).thenReturn(new IncomingNews());
 		
 		mockStatic(NewsReaderFactory.class);
 		when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(newsReader);
 	}
 
+	@Test
+	public void publicNewsTest () {
+        IncomingNews incomingNews = new IncomingNews();
+        incomingNews.add(new IncomingInfo("Subsciption type A.", SubsciptionType.A));
+        incomingNews.add(new IncomingInfo("Subsciption type C.", SubsciptionType.C));
+        incomingNews.add(new IncomingInfo("Subsciption non type.", SubsciptionType.NONE));
+
+        when(newsReader.read()).thenReturn(incomingNews);
+        NewsLoader newsLoader = new NewsLoader();
+        PublishableNews publishableNews = newsLoader.loadNews();
+        assertThat(publishableNews.getPublicContent().size(), is(1));
+	}
 }
