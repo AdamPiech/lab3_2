b10cadc5d331b701a2ab16ea49714b36b9808eb4
Adam Piech <adampiech7@op.pl>
2016-04-17 18:29:31 +0200

    Modyfikacja pom.xml, dodanie pliku NewsLoaderTest.

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,18 @@
+package edu.iis.mto.staticmock;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NewsLoaderTest {
+
+    private ConfigurationLoader configurationLoader;
+    private NewsReaderFactory newsReader;
+	
+	@Before
+	public void test() {
+		
+	}
+
+}
