48ac3bd447b166bdf5606d6f8b639d45affccbe9
Adam Piech <adampiech7@op.pl>
2016-04-17 18:57:43 +0200

    Dodano mocki

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -1,18 +1,28 @@
 package edu.iis.mto.staticmock;
 
-import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.mockito.Mockito;
+
+import edu.iis.mto.staticmock.reader.NewsReader;
 
 public class NewsLoaderTest {
 
     private ConfigurationLoader configurationLoader;
-    private NewsReaderFactory newsReader;
 	
 	@Before
-	public void test() {
+	public void start() {
+		configurationLoader = mock(ConfigurationLoader.class);
+		when(configurationLoader.loadConfiguration()).thenReturn(new Configuration());
+		mockStatic(ConfigurationLoader.class);
+		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
 		
+		NewsReader newsReader = mock(NewsReader.class);
+		when(newsReader.read()).thenReturn(new IncomingNews());
+		
+		mockStatic(NewsReaderFactory.class);
+		when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(newsReader);
 	}
 
 }
